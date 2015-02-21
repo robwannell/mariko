6f7ee0a9a61e8eb2ceec706bb649ca97ce5ebc0b
robwannell <rob@meltemiweb.com>
2015-02-21 14:55:33 -0800

    secrets

--- Capfile
+++ Capfile
@@ -19,9 +19,9 @@ require 'capistrano/bundler'
 require 'capistrano/rails'
 
 # If you are using rbenv add these lines:
-require 'capistrano/rbenv'
- set :rbenv_type, :user # or :system, depends on your rbenv setup
- set :rbenv_ruby, '2.1.5'
+#require 'capistrano/rbenv'
+# set :rbenv_type, :user # or :system, depends on your rbenv setup
+# set :rbenv_ruby, '2.1.5'
 # require 'capistrano/rvm'
 # require 'capistrano/rbenv'
 # require 'capistrano/chruby'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,3 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
 
 set :application, 'mariko'
 set :repo_url, 'git@github.com:robwannell/mariko.git'
@@ -8,7 +6,7 @@ set :deploy_to, '/home/deploy/mariko'
 
 set :bundle_flags,    ""
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
