8f3f3308bd5c4d6f90980f53e0967517961817d6
robwannell <rob@meltemiweb.com>
2015-03-05 11:05:38 -0800

    c

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :deploy_to, '/home/deploy/mariko'
 
 set :bundle_flags,    ""
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/initializers/setup_mail.rb}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
