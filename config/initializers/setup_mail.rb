47c98e1cbd1836b29c141e00e5f4332cf7f1eb2c
robwannell <rob@meltemiweb.com>
2015-03-04 16:23:20 -0800

    gma

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,7 +2,6 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
 :address => "smtp.gmail.com",
 :port => 587,
-:domain => 'yamadaforsenate.com',
 :user_name => ENV["MAILER_USERNAME"],
 :password => ENV["MAILER_PW"],
 :authentication => "plain",
