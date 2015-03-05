550f1da9f2b6ca7f83c273c34726c7a7a13fee86
robwannell <rob@meltemiweb.com>
2015-03-05 09:44:50 -0800

    sendy

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'notifier@yamadaforsenate.com',
-  :password => ENV["MAILER_PW"],
+  :password => ENV['MAILER_PW'],
   :domain => 'meltemiweb.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
