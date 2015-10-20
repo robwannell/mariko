004cc7f7b929f9c4f4debb34ee12a08ab1038436
robwannell <rob@meltemiweb.com>
2015-10-20 14:49:00 -0700

    Med

--- app/models/media_pic.rb
+++ app/models/media_pic.rb
@@ -3,7 +3,7 @@ class MediaPic < ActiveRecord::Base
    # Validate content type
    validates_attachment_content_type :image, :content_type => /\Aimage/
    # Validate filename
-   validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
+   validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /JPE?G\Z/]
   has_attached_file :image, :styles => { :standard => "820x600>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   
    default_scope  {order 'rank ASC'}
